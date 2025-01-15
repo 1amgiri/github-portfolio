b4768d41ef27a288429cd2aa4ab55570c5bf4ab4
GIRI <yadavgirish406@gmail.com>
2025-01-15 15:49:23 +0530

    third commit

--- src/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="bg-dark text-white text-center py-3">
-    <p>&copy; 2025 Giri's Portfolio. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
